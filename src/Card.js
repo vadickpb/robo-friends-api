72e5f3a39b7fc04afb0aee4777154a000f4ece58
vadickpb <vadick1@gmail.com>
2020-10-25 15:22:17 -0500

    creando el componente card

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Card = (props) => {
+
+    const { id, name, email} = props;
+    return (
+        <div className= "bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+
+            <img src ={`https://robohash.org/${id}?200x200`} alt= 'robots'/>
+            <h2>{ name }</h2>
+            <p>{ email }</p>
+
+        </div>
+    )
+    
+}
+
+export default Card;
