db85aca2d5884a50036bf25ea0d3851d33d9856a
vadickpb <vadick1@gmail.com>
2020-10-25 16:24:25 -0500

    component CardList

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const Card = (props) => {
+export const Card = ({ id, name, email} ) => {
 
-    const { id, name, email} = props;
+    
     return (
         <div className= "bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Card from './Card';
+import CardList from './CardList';
 import {robots} from './robots'
 
 import reportWebVitals from './reportWebVitals';
@@ -10,11 +11,7 @@ import './index.css';
 import 'tachyons'
 
 ReactDOM.render(
-  <div>
-    <Card id = { robots[0].id } name ={ robots[0].name} email = { robots[0].email}/>
-    <Card id = { robots[1].id } name ={ robots[1].name} email = { robots[1].email}/>
-    <Card id = { robots[2].id } name ={ robots[2].name} email = { robots[2].email}/>
-  </div>,
+  <CardList robots = {robots}/>,
   document.getElementById('root')
 );
 
